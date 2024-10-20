4e38f5063da2aebc4c9d496a256dcd6cbaee1867
Fernando Petros <fernandopetros@gmail.com>
2024-10-20 16:57:16 +0100

    adjusted git colors and set terminal color variable

--- git.sh
+++ git.sh
@@ -9,13 +9,13 @@ git config --global color.ui true
 
 git config --global color.diff-highlight.oldNormal    "#C34043"
 git config --global color.diff-highlight.oldHighlight "#C34043 #43242B bold"
-git config --global color.diff-highlight.newNormal    "#76946A"
-git config --global color.diff-highlight.newHighlight "#76946A #2B3328 bold"
+git config --global color.diff-highlight.newNormal    "#98BB6C"
+git config --global color.diff-highlight.newHighlight "#98BB6C #2B3328 bold"
 
 git config --global color.diff.meta       "#DCA561"
 git config --global color.diff.frag       "#957FB8 bold"
 git config --global color.diff.func       "#7E9CD8 bold"
 git config --global color.diff.commit     "#DCA561 bold"
 git config --global color.diff.old        "#C34043 bold"
-git config --global color.diff.new        "#76946A bold"
+git config --global color.diff.new        "#98BB6C bold"
 git config --global color.diff.whitespace "#DCA561 reverse"
