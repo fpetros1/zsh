75d9367ef0a31a53e29f58d441a268943a6504e8
Fernando Villar <fernando.frangos.villar@rabobank.nl>
2024-10-20 17:02:35 +0100

    add macos dependencies to install script

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ CONFIG_DIR=$(realpath $(dirname $0))
     mv "$HOME/.zshrc" "$HOME/.zshrc.old"
 
 ln -s "$CONFIG_DIR/rc.zsh" "$HOME/.zshrc"
+
+[[ $(uname) == "Darwin" ]] && [[ $(which brew) ]] &&\
+    brew install gawk grep gnu-sed coreutils
